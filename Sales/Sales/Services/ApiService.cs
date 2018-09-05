4da51ef1fcc93a840697c03d361d6687e6dac5b6
LENOVO-PC\Luis Uribe <luisuribe9@hotmail.com>
2018-09-05 08:17:40 -0500

    Validacion
    -Validacion conexion a internet
    -Se agregan nuggets que contiene el plugin para la validacion del internet
    -Key para almacenar la url de la api weben el directorio

--- Sales/Sales/Services/ApiService.cs
+++ Sales/Sales/Services/ApiService.cs
@@ -1,6 +1,4 @@
-﻿
-
-namespace Sales.Services
+﻿namespace Sales.Services
 {
     using System;
     using System.Collections.Generic;
@@ -8,9 +6,38 @@ namespace Sales.Services
     using System.Threading.Tasks;
     using Common.Models;
     using Newtonsoft.Json;
+    using Plugin.Connectivity;
 
     public class ApiService
     {
+        public async Task<Response> CheckConecction()
+        { //valida que el dispositivo tenga el wifi activo 
+            if (!CrossConnectivity.Current.IsConnected)
+            {
+                return new Response
+                {
+                    IsSucces = false,
+                    Message = "Please turn on your internet settings"
+                };
+            }
+
+            var isRecheable = await CrossConnectivity.Current.IsRemoteReachable("google.com");
+            if (!isRecheable)
+            {
+                return new Response
+                {
+                    IsSucces = false,
+                    Message = "No internet connecction"
+                };
+            }
+
+            return new Response
+            {
+                IsSucces = true
+            };
+        }
+
+
         public async Task<Response> GetList<T>(string urlBase, string prefix, string controller)
         {
             try
--- Sales/Sales/ViewModels/ProductsViewModel.cs
+++ Sales/Sales/ViewModels/ProductsViewModel.cs
@@ -3,13 +3,17 @@
     using System;
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
+    using System.Windows.Input;
     using Common.Models;
+    using GalaSoft.MvvmLight.Command;
     using Sales.Services;
     using Xamarin.Forms;
 
     public class ProductsViewModel : BaseViewModel
     {
         private ApiService apiService;
+
+        private bool isRefreshing;
 
         private ObservableCollection<Product> products;
 
@@ -19,6 +23,11 @@
             set { this.SetValue(ref this.products, value); }
         }
 
+        public bool IsRefreshing
+        {
+            get { return this.isRefreshing; }
+            set { this.SetValue(ref this.isRefreshing, value); }
+        }
         public ProductsViewModel()
         {
             this.apiService = new ApiService();
@@ -27,14 +36,35 @@
 
         private async void LoadProducts()
         {
-            var response = await this.apiService.GetList<Product>("https://salesapiservices.azurewebsites.net", "/api", "/Products");
+            this.IsRefreshing = true;
+
+            var conecction = await this.apiService.CheckConecction();
+            if (!conecction.IsSucces)
+            {
+                this.IsRefreshing = false;
+                await Application.Current.MainPage.DisplayAlert("Error", conecction.Message, "Accept");
+                return;
+            }
+            var url = Application.Current.Resources["UrlApi"].ToString();
+            var response = await this.apiService.GetList<Product>(url, "/api", "/Products");
             if (!response.IsSucces)
             {
+                this.IsRefreshing = false;
                 await Application.Current.MainPage.DisplayAlert("Error", response.Message, "Accept");
                 return;
             }
             var list = (List<Product>)response.Result;
             this.Products = new ObservableCollection<Product>(list);
+            this.IsRefreshing = false;
+        }
+
+        public ICommand RefreshCommand
+        {
+            get
+            {
+                return new RelayCommand(LoadProducts);
+            }
+
         }
     }
 }
